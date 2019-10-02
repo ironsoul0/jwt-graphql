ffa5c48f9bae9766a85c1b34ed320a35fe7d7ff7
ironsoul <timka2609@gmail.com>
2019-10-02 16:04:43 +0600

    Add type-graphql

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,29 @@
 import "reflect-metadata";
-import { createConnection } from "typeorm";
-import { User } from "./entity/User";
+//import { createConnection } from "typeorm";
+//import { User } from "./entity/User";
 import express from 'express';
-
-const app = express();
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from "type-graphql";
+import { UserResolver } from "./UserResolver";
 
 const bootstrap = async () => {
+  const app = express();
 
+  app.get('/', (_, res) => {
+    res.send('hello');
+  });
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [UserResolver]
+    })
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("Server was started");
+  });
 }
 
 bootstrap();
